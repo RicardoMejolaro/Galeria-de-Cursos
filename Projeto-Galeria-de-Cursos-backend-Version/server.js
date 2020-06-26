508b1f865fd2780fabf9ef08514eb182e867d2ed
RicardoMejolaro <56407810+RicardoMejolaro@users.noreply.github.com>
2020-06-26 11:09:11 -0300

    challenges 3-1 3-2 and 3-3 completed and responsive layout added

---
+++ Projeto-Galeria-de-Cursos-backend-Version/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const courses = require("./data-courses");
+const about = require("./data-about");
+
+const server = express();
+
+server.use(express.static('public'));
+
+server.set('view engine', 'njk');
+
+nunjucks.configure('views', {
+  express: server,
+  autoescape: false,
+  noCache: true
+});
+
+server.get('/', (req, res) => {
+  return res.render('courses', { courses });
+});
+
+server.get('/about', (req, res) => {
+  return res.render('about', { about });
+});
+
+
+server.get("/:id", (req, res) => {
+  const id = req.params.id;
+  
+  const course = courses.find( course => course.id == id );
+
+  if(!course) {
+    return res.send("Conteúdos não encontrado!");  
+  } 
+
+  return res.render('course-details', { course })
+});
+
+server.use( (req, res) => {
+  res.status(404).render("not-found");
+});
+
+server.listen(3000, () => {
+  console.log("Server is Running!");
+});
